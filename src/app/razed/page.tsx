69ddc963a18bb168037631af864dddb8548bd9a7
kleosr <kleosr@proton.me>
2025-11-01 11:09:00 -0400

    Changed Metadata to be async, requested by Nick.

--- src/app/razed/page.tsx
+++ src/app/razed/page.tsx
@@ -3,12 +3,14 @@ import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import { createMetadata } from "@/../lib/metadata";
 import RazedLeaderboard from "@/components/Razed/RazedLeaderboard";
 
-export const metadata: Metadata = createMetadata({
-  title: "50k Razed Leaderboard",
-  description: "50k Razed Leaderboard - Compete for $50,000 in monthly prizes",
-  path: "/razed",
-  image: "/images/cover/Razed_Leaderboard.png",
-});
+export async function generateMetadata(): Promise<Metadata> {
+  return createMetadata({
+    title: "50k Razed Leaderboard",
+    description: "50k Razed Leaderboard - Compete for $50,000 in monthly prizes",
+    path: "/razed",
+    image: "/images/cover/Razed_Leaderboard.png",
+  });
+}
 
 export default function Razed() {
   return (
